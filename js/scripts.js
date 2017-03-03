ae11ad9c23a6446360f2ef405e42ba8df0d92172
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-03-03 11:14:49 -0800

    add multiple options for topping with associated prices in price prototype-up to 10 toppings

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,7 @@ Pizza.prototype.price = function(toppings, size){
     var numberOfToppings = i;
     console.log(i);
   };
-  if ((numberOfToppings === 2) && (size === "large")) {
-    console.log("2 topping")
-    return largePizzaPrice + 3;
-  } else if ((numberOfToppings === 1) && (size === "medium")) {
+    if ((numberOfToppings === 1) && (size === "medium")) {
     console.log("I'm here")
     return mediumPizzaPrice + 2;
   } else if ((numberOfToppings === 1) && (size === "small")) {
@@ -23,6 +20,87 @@ Pizza.prototype.price = function(toppings, size){
   } else if ((numberOfToppings === 1) && (size === "large")) {
     console.log("I'm here")
     return largePizzaPrice + 2;
+  } else if ((numberOfToppings === 2) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 3;
+  } else if ((numberOfToppings === 2) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 3;
+  } else if ((numberOfToppings === 2) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 3;
+  } else if ((numberOfToppings === 3) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 4;
+  } else if ((numberOfToppings === 3) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 4;
+  } else if ((numberOfToppings === 3) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 4;
+  } else if ((numberOfToppings === 4) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 5;
+  } else if ((numberOfToppings === 4) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 5;
+  } else if ((numberOfToppings === 4) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 5;
+  } else if ((numberOfToppings === 5) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 6;
+  } else if ((numberOfToppings === 5) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 6;
+  } else if ((numberOfToppings === 5) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 6;
+  } else if ((numberOfToppings === 6) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 7;
+  } else if ((numberOfToppings === 6) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 7;
+  } else if ((numberOfToppings === 6) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 7;
+  } else if ((numberOfToppings === 7) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 8;
+  } else if ((numberOfToppings === 7) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 8;
+  } else if ((numberOfToppings === 7) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 8;
+  } else if ((numberOfToppings === 8) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 9;
+  } else if ((numberOfToppings === 8) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 9;
+  } else if ((numberOfToppings === 8) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 9;
+  } else if ((numberOfToppings === 9) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 10;
+  } else if ((numberOfToppings === 9) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 10;
+  } else if ((numberOfToppings === 9) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 10;
+  } else if ((numberOfToppings === 10) && (size === "small")) {
+    console.log("I'm here");
+    return smallPizzaPrice + 11;
+  } else if ((numberOfToppings === 10) && (size === "medium")) {
+    console.log("I'm here")
+    return mediumPizzaPrice + 11;
+  } else if ((numberOfToppings === 10) && (size === "large")) {
+    console.log("2 topping")
+    return largePizzaPrice + 11;
   };
 };
 
