78761ce94635cb8fc5604334cc133af4d98f6d82
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-03-03 15:48:56 -0800

    add more logic, debugged an issue with a validation alert

--- js/scripts.js
+++ js/scripts.js
@@ -13,28 +13,11 @@ Pizza.prototype.price = function(toppings, size){
   smallPizzaPrice = 12
   mediumPizzaPrice = 15
   largePizzaPrice = 18
-  debugger;
   for (i=1;i<=toppings.length;i++){
     var numberOfToppings = i;
   };
-    if (size === undefined) {
-      alert("Please choose a pizza size to submit an order.");
-  } else if ((size === "personal") || (size === "small") ||  (size === "medium") || (size === "large") && (numberOfToppings === undefined)) {
-      confirm("Are you sure you don't want any toppings?")
-      if (size === "personal"){
-        return personalPizzaPrice;
-      } else if (size === "small") {
-        return smallPizzaPrice;
-      } else if (size === "medium") {
-        return mediumPizzaPrice;
-      } else if (size === "large") {
-        console.log("I'm here")
-        return largePizzaPrice;
-        }
-  } else if ((size === "personal") && (numberOfToppings > 1)) {
+    if ((size === "personal") && (numberOfToppings > 1)) {
       alert("Please only choose 1 topping for your personal size pizza.");
-  } else if (size === "personal") {
-      return personalPizzaPrice;
   } else if ((numberOfToppings === 1) && (size === "medium")) {
       return mediumPizzaPrice + 2;
   } else if ((numberOfToppings === 1) && (size === "small")) {
@@ -95,6 +78,14 @@ Pizza.prototype.price = function(toppings, size){
       return mediumPizzaPrice + 11;
   } else if ((numberOfToppings === 10) && (size === "large")) {
       return largePizzaPrice + 11;
+  } else if (size === "personal") {
+      return personalPizzaPrice;
+  } else if (size === "small") {
+      return smallPizzaPrice;
+  } else if (size === "medium") {
+      return mediumPizzaPrice;
+  } else if (size === "large") {
+      return largePizzaPrice;
   };
 };
 
